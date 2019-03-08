930c5cec7190ca0da6b945429126f98859873f9b
TheCrether <denisimeri6@gmail.com>
2019-03-08 23:52:06 +0100

    animations on fleek

---
+++ js/index.js
@@ -0,0 +1,58 @@
+let swipe = (delta) => {
+    if (delta > 0) {
+        const before = document.getElementsByClassName("scroll")[0];
+        before.id = "afterScroll";
+        const nav = document.getElementsByTagName("nav")[0];
+        nav.className = nav.className.replace("unscrolled", "scrolled");
+        const content = document.getElementById("content-unscrolled");
+        content.style.display = "block";
+        setTimeout(() => {
+            content.id = "content-scrolled";
+        }, 11);
+    }
+};
+
+document.addEventListener("wheel", (e) => {
+    swipe(e.deltaY);
+});
+
+//animate scrollDown PFEILE
+let pfeile = document.getElementsByClassName('fas fa-angle-right');
+let pfeilBool = 0;
+let changePfeil = () => {
+    if (pfeilBool == 0) {
+        pfeile[0].style.color = "#fff";
+        pfeile[1].style.color = "#fff";
+        pfeilBool++;
+    } else if (pfeilBool == 1) {
+        pfeile[0].style.color = "#631175";
+        pfeilBool++;
+    } else if (pfeilBool == 2) {
+        pfeile[1].style.color = "#631175";
+        pfeilBool++;
+    } else if (pfeilBool == 3) {
+        pfeile[0].style.color = "#fff";
+        pfeilBool = 0;
+    }
+}
+setInterval(function () {
+    changePfeil();
+}, 800);
+
+let randomColor = () => {
+    const randLets = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += randLets[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+let ciscodisco = () => {
+    setInterval(function () {
+        const elems = document.body.getElementsByTagName("*");
+        for (let item of elems) {
+            item.style.backgroundColor = randomColor();
+        }
+    }, 500);
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const home = document.getElementById("kaindorfText");
+home.addEventListener("click", () => {
+    window.location.href = "/";
+    return false;
+});
