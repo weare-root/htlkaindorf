e285ec7a270cba5a12643ee9525197b76d8ba49d
TheCrether <denisimeri6@gmail.com>
2019-03-11 22:29:33 +0100

    moved things
    moved some css rules to main and made  a normal nav

--- js/index.js
+++ js/index.js
@@ -2,62 +2,49 @@ let swipeTrue = true;
 
 //ON SWIPE
 let swipe = (delta) => {
-    if (delta > 0 && swipeTrue) {
-        const before = document.getElementsByClassName("scroll")[0];
-        before.id = "afterScroll";
+	if (delta > 0 && swipeTrue) {
+		const before = document.getElementsByClassName('scroll')[0];
+		before.id = 'afterScroll';
 
-        if (window.innerWidth < 600) {
-            const kdText = document.getElementById("kaindorfText");
-            kdText.id = kdText.id + "-scrolled";
-        }
+		if (window.innerWidth < 600) {
+			const kdText = document.getElementById('kaindorfText');
+			kdText.id = kdText.id + '-scrolled';
+		}
 
-        const nav = document.getElementsByTagName("nav")[0];
-        nav.className = nav.className.replace("unscrolled", "scrolled");
-        const content = document.getElementById("content-unscrolled");
-        content.style.display = "block";
-        setTimeout(() => {      
-            content.id = "content-scrolled";
-        }, 11);
+		const nav = document.getElementsByTagName('nav')[0];
+		nav.className = nav.className.replace('unscrolled', 'scrolled');
+		const content = document.getElementById('content-unscrolled');
+		content.style.display = 'block';
+		setTimeout(() => {
+			content.id = 'content-scrolled';
+		}, 400);
 
-        const abts = document.getElementsByClassName("abt");
-        for (let abt of abts) {
-            abt.id = abt.id + "-scrolled";
-        }
-        swipeTrue = false;
-    }
+		const abts = document.getElementsByClassName('abt');
+		for (let abt of abts) {
+			abt.id = abt.id + '-scrolled';
+		}
+
+		const navNormal = document.getElementsByClassName('nav-normal')[0];
+		navNormal.className = navNormal.className + '-scrolled';
+		swipeTrue = false;
+	}
 };
 //ON RESIZE
-window.addEventListener("resize", () => {
-    let kdText = document.getElementById('kaindorfText') || document.getElementById("kaindorfText-scrolled");
+window.addEventListener('resize', () => {
+	let kdText = document.getElementById('kaindorfText') || document.getElementById('kaindorfText-scrolled');
 
-    if (!swipeTrue && window.innerWidth < 600) {
-        if (!kdText.id.endsWith("-scrolled")) {
-            kdText.id = kdText.id + "-scrolled";
-        }
-    } else {
-        kdText.id = kdText.id.replace("-scrolled", "");
-    }
+	if (!swipeTrue && window.innerWidth < 600) {
+		if (!kdText.id.endsWith('-scrolled')) {
+			kdText.id = kdText.id + '-scrolled';
+		}
+	} else {
+		kdText.id = kdText.id.replace('-scrolled', '');
+	}
 });
 
-document.addEventListener("wheel", (e) => {
-    swipe(e.deltaY);
+document.addEventListener('wheel', (e) => {
+	swipe(e.deltaY);
 });
-
-//ABTS HOVER
-const abts = document.getElementsByClassName("abt");
-for (let abt of abts) {
-    abt.addEventListener("mouseover", (e) => {
-        let query = "#" + abt.id + " .abtName";
-        const name = document.querySelector(query);
-        name.className = name.className + "-hovered";
-    });
-    abt.addEventListener("mouseout", (e) => {
-        let query = "#" + abt.id + " .abtName-hovered";
-        const name = document.querySelector(query);
-        name.className = name.className.replace("-hovered", "");
-    });
-}
-
 
 /*
  *   NOT NECESSARY TO WOKK PROPERLY
@@ -67,40 +54,40 @@ for (let abt of abts) {
 let pfeile = document.getElementsByClassName('fas fa-angle-right');
 let pfeilBool = 0;
 let changePfeil = () => {
-    if (pfeilBool == 0) {
-        pfeile[0].style.color = "#fff";
-        pfeile[1].style.color = "#fff";
-        pfeilBool++;
-    } else if (pfeilBool == 1) {
-        pfeile[0].style.color = "#631175";
-        pfeilBool++;
-    } else if (pfeilBool == 2) {
-        pfeile[1].style.color = "#631175";
-        pfeilBool++;
-    } else if (pfeilBool == 3) {
-        pfeile[0].style.color = "#fff";
-        pfeilBool = 0;
-    }
+	if (pfeilBool == 0) {
+		pfeile[0].style.color = '#fff';
+		pfeile[1].style.color = '#fff';
+		pfeilBool++;
+	} else if (pfeilBool == 1) {
+		pfeile[0].style.color = '#631175';
+		pfeilBool++;
+	} else if (pfeilBool == 2) {
+		pfeile[1].style.color = '#631175';
+		pfeilBool++;
+	} else if (pfeilBool == 3) {
+		pfeile[0].style.color = '#fff';
+		pfeilBool = 0;
+	}
 };
-setInterval(function () {
-    changePfeil();
+setInterval(function() {
+	changePfeil();
 }, 800);
 
 //CISCODISCO STUFF
 let randomColor = () => {
-    const randLets = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += randLets[Math.floor(Math.random() * 16)];
-    }
-    return color;
+	const randLets = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += randLets[Math.floor(Math.random() * 16)];
+	}
+	return color;
 };
 
 let ciscodisco = () => {
-    setInterval(function () {
-        const elems = document.body.getElementsByTagName("*");
-        for (let item of elems) {
-            item.style.backgroundColor = randomColor();
-        }
-    }, 500);
+	setInterval(function() {
+		const elems = document.body.getElementsByTagName('*');
+		for (let item of elems) {
+			item.style.backgroundColor = randomColor();
+		}
+	}, 500);
 };
--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ home.addEventListener("click", () => {
     window.location.href = "/";
     return false;
 });
+
+//ABTS HOVER
+const abts = document.getElementsByClassName("abt");
+for (let abt of abts) {
+    abt.addEventListener("mouseover", (e) => {
+        let query = "#" + abt.id + " .abtName";
+        const name = document.querySelector(query);
+        name.className = name.className + "-hovered";
+    });
+    abt.addEventListener("mouseout", (e) => {
+        let query = "#" + abt.id + " .abtName-hovered";
+        const name = document.querySelector(query);
+        name.className = name.className.replace("-hovered", "");
+    });
+}
